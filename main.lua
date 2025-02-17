8a847a827a1738abc37a10f6659046519641976b
jamied <jamiw1@pm.me>
2025-02-17 00:43:15 -0500

    remove ai

--- main.lua
+++ main.lua
@@ -1,28 +1,54 @@
-local NeuralNetwork = require("network")
+local neuron = require("neuron")
+local layer = require("layer")
+local network = require("network")
 
-local nn = NeuralNetwork.new(2, {4, 3}, 1)  -- 2 inputs, 2 hidden layers (4 and 3 nodes), 1 output
-nn:setLearningRate(0.1)
+math.randomseed(os.time())
 
+function createNetwork(inputcount, neuroncounts)
+    local layers = {}
+    local previous_count = inputcount
+
+    for _, count in ipairs(neuroncounts) do
+        local neurons = {}
+        for i = 1, count do
+            local weights = {}
+            for j = 1, previous_count do
+                table.insert(weights, math.random())
+            end
+            local bias = math.random()
+            table.insert(neurons, neuron.new(bias, weights))
+        end
+        table.insert(layers, layer.new(neurons))
+        previous_count = count
+    end
+
+    return network.new(layers)
+end
 
 local trainingData = {
-    {{1, 0}, {1}},
+    {{0, 0}, {0}},
     {{0, 1}, {1}},
-    {{1, 1}, {0}},
-    {{0, 0}, {0}}
+    {{1, 0}, {1}},
+    {{1, 1}, {0}}
 }
--- Training
+
+local inputs = {1, 1}
+local neuroncounts = {3, 1}
+local mynetwork = createNetwork(#inputs, neuroncounts)
+
 for i=1, 10000 do
-    nn:train(trainingData)
+    mynetwork:train(trainingData, 0.1)
     if i % 1000 == 0 then
-        print("Epoch: "..i.." Error: "..nn:error({1, 0}, {1}))
+        print(mynetwork:error(inputs, {0}))
     end
 end
 
--- Prediction
-local result = nn:feedForward({1, 0})
-print(result[1])
-local result = nn:feedForward({0, 1})
-print(result[1])
+print(mynetwork:pass(inputs)[1])
+print(mynetwork:error(inputs, {0.9}))
+print(mynetwork:error(inputs, {1.2}))
+print(mynetwork:error(inputs, {0.3}))
 
-
-return NeuralNetwork
+--print(mynetwork:pass({3})[1])
+--print(mynetwork:pass({4})[1])
+--print(mynetwork:pass({5})[1])
+--print(mynetwork:pass({6})[1])
